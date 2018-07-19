4be7c33598ed770d12f2eb1187852f0348ad0771
YadiraF <sheldol.f@live.com>
2018-07-19 10:19:49 +0800

    add texture render

--- examples/5_render.py
+++ examples/5_render.py
@@ -19,7 +19,7 @@ from face3d.morphable_model import MorphabelModel
 C = sio.loadmat('Data/example1.mat')
 vertices = C['vertices']; colors = C['colors']; triangles = C['triangles']
 colors = colors/np.max(colors)
-# move center to [0,0,0]
+# transform
 vertices = vertices - np.mean(vertices, 0)[np.newaxis, :]
 s = 180/(np.max(vertices[:,1]) - np.min(vertices[:,1]))
 R = mesh.transform.angle2matrix([0, 0, 0]) 
@@ -29,17 +29,7 @@ vertices = mesh.transform.similarity_transform(vertices, s, R, t)
 h = w = 256
 image_vertices = mesh.transform.to_image(vertices, h, w)
 
-# -----------------------------------------render
-# # render texture python
-# st = time()
-# rendering_tp = face3d.mesh.render.render_texture(vertices, triangles, texture, texcoord, triangles, h, w)
-# print('----------texture python: ', time() - st)
-
-# # render texture c++
-# st = time()
-# rendering_tc = face3d.mesh_cython.render.render_texture(vertices, triangles, texture, texcoord, triangles, h, w)
-# print('----------texture c++: ', time() - st)
-
+# -----------------------------------------render colors
 # render colors python
 st = time()
 rendering_cp = face3d.mesh.render.render_colors(image_vertices, triangles, colors, h, w)
@@ -54,3 +44,37 @@ print('----------colors python ras: ', time() - st)
 st = time()
 rendering_cc = face3d.mesh_cython.render.render_colors(image_vertices, triangles, colors, h, w)
 print('----------colors c++: ', time() - st)
+
+# ----------------------------------------- render texture(texture mapping)
+# when face texture is saved as texture map, this method is recommended.
+# load texture
+texture = io.imread('Data/uv_texture_map.jpg')/255.
+tex_h, tex_w, _ = texture.shape
+# load texcoord(uv coords)
+uv_coords = face3d.morphable_model.load.load_uv_coords('Data/BFM/Out/BFM_UV.mat') #
+uv_coords[:,0] = uv_coords[:,0]*(tex_h - 1)
+uv_coords[:,1] = uv_coords[:,1]*(tex_w - 1)
+uv_coords[:,1] = tex_w - uv_coords[:,1] - 1
+texcoord = np.hstack((uv_coords, np.zeros((uv_coords.shape[0], 1)))) # add z# render texture python
+# tex_triangles
+tex_triangles = triangles 
+
+# render texture python
+st = time()
+rendering_tp = face3d.mesh.render.render_texture(image_vertices, triangles, texture, texcoord, tex_triangles, h, w)
+print('----------texture python: ', time() - st)
+
+# render texture c++
+st = time()
+rendering_tc = face3d.mesh_cython.render.render_texture(image_vertices, triangles, texture, texcoord, tex_triangles, h, w)
+print('----------texture c++: ', time() - st)
+plt.subplot(2,2,1)
+plt.imshow(rendering_cp)
+plt.subplot(2,2,2)
+plt.imshow(rendering_cc)
+plt.subplot(2,2,3)
+plt.imshow(rendering_tp)
+plt.subplot(2,2,4)
+plt.imshow(rendering_tc)
+plt.show()
+
